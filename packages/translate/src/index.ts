f5381fd0f39a724a04db95e16aa01a63c51b1dae
Axel Lavoie <axel.lavoie@gmail.com>
2022-04-12 12:00:55 +0200

    fix build

--- packages/translate/src/index.ts
+++ packages/translate/src/index.ts
@@ -1,8 +1,10 @@
 import TypesafeI18n from './i18n/i18n-react'
+import * as i18nUtilsAsync from './i18n/i18n-util.async'
+import * as i18nUtilsSync from './i18n/i18n-util.sync'
 
 export * from './i18n/formatters'
 export * from './i18n/i18n-react'
 export * from './i18n/i18n-types'
 export * from './i18n/i18n-util'
 export * from './i18n/translation'
-export { TypesafeI18n }
+export { TypesafeI18n, i18nUtilsAsync, i18nUtilsSync }
